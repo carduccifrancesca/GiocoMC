369734de777c1bb3049e92f892a547e5b3b34f24
martinaluciani <martina00.luciani@virgilio.it>
2022-03-25 12:33:03 +0100

    modifiche
    piccione

---
+++ Assets/Levels/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiccioneLoop : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float leftLimit;
+    [SerializeField] private float rightLimit;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(speed * Time.deltaTime, 0);
+
+        if (transform.position.x < -leftLimit)
+        {
+            transform.position = new Vector3(rightLimit, transform.position.y);
+        }
+
+    }
+}
+
