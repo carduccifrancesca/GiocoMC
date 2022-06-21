04527754a1a9191f0f2f62d1ca9d973244113041
CassaJutsuFlessibile <91905077+CassaJutsuFlessibile@users.noreply.github.com>
2022-06-21 17:57:41 +0200

    Aggiunto suono background

--- Assets/Levels/Scripts/Portabiglietti.cs
+++ Assets/Levels/Scripts/Portabiglietti.cs
@@ -7,12 +7,15 @@ public class Portabiglietti : MonoBehaviour
 {
     int biglietti = 0;
     public TextMeshProUGUI contaBiglietti;
+    [SerializeField] private AudioSource suonoBigliettoRaccolto;
 
     // Update is called once per frame
     public void OnTriggerEnter2D(Collider2D collision)
     {
+        suonoBigliettoRaccolto.Play();
         if (collision.name.Contains("atac"))
         {
+         
             biglietti++;
             collision.gameObject.SetActive(false);
             AggiornaBiglietti();
